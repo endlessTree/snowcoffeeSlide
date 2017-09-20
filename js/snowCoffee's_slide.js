2372a70b15231421e4f679cd508f6f3f23763283
EndlessTree <sksmsqhzjf@naver.com>
2017-09-20 13:35:15 +0900

    Add files via upload

--- js/snowCoffee's_slide.js
+++ js/snowCoffee's_slide.js
@@ -1,40 +1,31 @@
 (function($) {
   $.snowCoffee = function() {
 
-    /*	변수	*/
+/*	변수	*/
 
-    // 각 요소의 선택자를 담은 변수 선언
-    var $sWrap = $('#slideWrap'),
+    // 각 요소의 선택자
+    var $sWrap = $('#slide_wrap'),
         $slide = $('.slide'),
         $sLi = $('.slide li'),
 
-        // slide에 필요한 갖가지 변수 선언
+        // slide 관련
         indexNow = 0, // 현재 slide li의 인덱스를 담을 변수
         indexLast = $sLi.last().index(), // slide li의 마지막 index를 담을 변수
         // slideWidth = '100%', // slide li 에 있는 이미지의 width값을 변수에, 혹은 특정 사이즈
-        slideWidth = '500px', 	// slide li 에 있는 이미지의 width값을 변수에, 혹은 특정 사이즈
-        slideTime = '3000', // 자동 재생 시간
+        slideWidth = '500px', // slide li 에 있는 이미지의 width값을 변수에, 혹은 특정 사이즈
+        slideTime = '3000', // 자동 slide 시간
 
         // 셋인터벌 관련
         toggle = false,
         handle = null;
 
-
-    /*	리셋	*/
-
-    window.onload = function() {
-      reset();
-      startInterval();
-    };
-
-
-    /*	함수	*/
+/*	함수	*/
 
     // size reset
     function reset() {
-      $sWrap.width(slideWidth).height($sLi.height());
-      $slide.width($sLi.width() * (indexLast + 1)); // .slide의 넓이 값을 .slide li의 갯수만큼 넓히기
       $sLi.width($sWrap.width());
+      $slide.width($sLi.width() * (indexLast + 1)); // .slide의 넓이 값을 .slide li의 갯수만큼 넓히기
+      $sWrap.width(slideWidth).height($sLi.height());
     };
 
     // slide 이전 이미지
@@ -59,7 +50,9 @@
 
     // slide 이동 함수
     function move() {
-      $slide.css({ transform: 'translate3d(-'+($sLi.width() * indexNow)+'px, 0, 0)' });
+      $slide.css({
+        transform: 'translate3d(-' + ($sLi.width() * indexNow) + 'px, 0, 0)'
+      });
       paging();
     };
 
@@ -78,30 +71,23 @@
     };
 
     //슬라이드 개수 만큼 페이징 자동 추가
-		$('.paging').append('<ul></ul>')
+    $('.paging').append('<ul></ul>')
     for (var i = 0; i <= indexLast; i++) {
-			$('.paging ul').append('<li></li>');
-		};
+      $('.paging ul').append('<li></li>');
+    };
     $('.paging li').eq(indexNow).addClass('choice');
 
-    // 터치 이벤트
-    $slide.swipe({
-      //Generic swipe handler for all directions
-      swipe: function(event, direction, distance, duration, fingerCount, fingerData) {
-        if (direction == "left") {
-          slidePrev();
-          stopInterval();
-        } else if (direction == "right") {
-          slideNext();
-          stopInterval();
-        };
-      },
-      //Default is 75px, set to 0 for demo so any distance triggers swipe
-      threshold: 0
-    });
+
+/*	리셋	*/
+
+    window.onload = function() {
+      $sWrap.width(slideWidth).height($sLi.height());
+      reset();
+      startInterval();
+    };
 
 
-    /*	이벤트	*/
+/*	이벤트	*/
 
     // ◀ ▶ 버튼
     $('.slideBtn div').click(function(e) {
@@ -115,21 +101,19 @@
     });
 
     // paging li 클릭 했을 때 이벤트
-    $('.paging li').click(function(e) {
+    $('.paging li').click(function() {
       indexNow = $(this).index();
       move();
       stopInterval();
-      e.preventDefault();
     });
 
     // 자동 재생 / 정지 버튼
-    $('.autoBtn div').click(function(e) {
+    $('.autoBtn div').click(function() {
       if ($(this).hasClass('btnStart') == true) {
         startInterval();
       } else {
         stopInterval();
       };
-      e.preventDefault();
     });
 
     function startInterval() {
@@ -154,15 +138,31 @@
       toggle = !toggle;
     };
 
+    // 터치 이벤트
+    $slide.swipe({
+      //Generic swipe handler for all directions
+      swipe: function(event, direction, distance, duration, fingerCount, fingerData) {
+        if (direction == "left") {
+          slidePrev();
+          stopInterval();
+        } else if (direction == "right") {
+          slideNext();
+          stopInterval();
+        };
+      },
+      //Default is 75px, set to 0 for demo so any distance triggers swipe
+      threshold: 0
+    });
+
     // transition end
-    $slide.on("transitionend webkittransitionend otransitionend mstransitionend", function(e){
-      if( indexNow == 0 ){
+    $slide.on("transitionend webkittransitionend otransitionend mstransitionend", function() {
+      if (indexNow == 0) {
         console.log(1)
       }
     });
 
 
-  /* 리사이징 */
+/* 리사이징 */
 
     // 리사이징 후의 이벤트를 위한 함수
     var waitForFinalEvent = (function() {
@@ -182,8 +182,10 @@
     $(window).resize(function() {
 
       if (slideWidth == '100%') {
-        $slide.addClass('stop').css({transform: 'translate3d(-'+($sLi.width() * indexNow)+'px, 0, 0)'});
         reset();
+        $slide.addClass('stop').css({
+          transform: 'translate3d(-' + ($sLi.width() * indexNow) + 'px, 0, 0)'
+        });
       };
       stopInterval();
 
@@ -191,7 +193,7 @@
       waitForFinalEvent(function() {
         $slide.removeClass('stop'); // 리사이징 되고난 후에 transition 시간 생성
         startInterval();
-      }, 0);
+      }, 10);
 
     });
 
